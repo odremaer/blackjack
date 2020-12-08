35fd06b3a8a639d4611fdaf295b89fb941d3d4fb
odremaer <mr.jeka0104@gmail.com>
2020-12-08 05:15:33 +0300

    removed cur_deck(cards on hands) from deck.rb)

---
+++ deck.rb
@@ -0,0 +1,83 @@
+class Hash
+  def sample(n)
+    Hash[to_a.sample(n)]
+  end
+end
+
+class Deck
+  attr_accessor :playable_deck
+  Deck = {
+    '2♠' => 2,
+    '2♥' => 2,
+    '2♣' => 2,
+    '2♦' => 2,
+    '3♠' => 3,
+    '3♥' => 3,
+    '3♣' => 3,
+    '3♦' => 3,
+    '4♠' => 4,
+    '4♥' => 4,
+    '4♣' => 4,
+    '4♦' => 4,
+    '5♠' => 5,
+    '5♥' => 5,
+    '5♣' => 5,
+    '5♦' => 5,
+    '6♠' => 6,
+    '6♥' => 6,
+    '6♣' => 6,
+    '6♦' => 6,
+    '7♠' => 7,
+    '7♥' => 7,
+    '7♣' => 7,
+    '7♦' => 7,
+    '8♠' => 8,
+    '8♥' => 8,
+    '8♣' => 8,
+    '8♦' => 8,
+    '9♠' => 9,
+    '9♥' => 9,
+    '9♣' => 9,
+    '9♦' => 9,
+    '10♠' => 10,
+    '10♥' => 10,
+    '10♣' => 10,
+    '10♦' => 10,
+    'J♠' => 10,
+    'J♥' => 10,
+    'J♣' => 10,
+    'J♦' => 10,
+    'D♠' => 10,
+    'D♥' => 10,
+    'D♣' => 10,
+    'D♦' => 10,
+    'K♠' => 10,
+    'K♥' => 10,
+    'K♣' => 10,
+    'K♦' => 10,
+    'A♠' => [1,11],
+    'A♥' => [1,11],
+    'A♣' => [1,11],
+    'A♦' => [1,11],
+  }
+  def initialize
+    @playable_deck = Deck.dup
+  end
+
+  def give_card
+    if @playable_deck.empty?
+      puts 'новая колода'
+      update_deck
+    end
+    card = @playable_deck.sample(1)
+    @playable_deck.delete(card.keys[0])
+    return card
+  end
+
+  protected
+
+  def update_deck
+    @playable_deck = Deck
+  end
+
+end
