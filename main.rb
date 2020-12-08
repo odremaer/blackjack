985fd1c6f44e83f35f8d94340688991bf3767489
odremaer <mr.jeka0104@gmail.com>
2020-12-08 09:15:02 +0300

    refactoring in main.rb

--- main.rb
+++ main.rb
@@ -49,6 +49,35 @@ class Interface
   end
 
   def game_end
+    show_results
+    check_if_someone_has_lost
+
+    print "Хотите сыграть еще раз?\n"\
+          "(0) - да, (1) - нет\n"
+    choice = gets.chomp
+    case choice
+    when '0'
+      @player.drop_cards
+      @dealer.drop_cards
+      game_start
+    when '1'
+      print "Выход\n"
+    end
+  end
+
+  def check_if_someone_has_lost
+    if @player.bank.zero?
+      print "У вас закончились деньги, вы проиграли\n"
+      return nil
+    end
+
+    if @dealer.bank.zero?
+      print "У диллера закончились деньги, вы выиграли!\n"
+      return nil
+    end
+  end
+
+  def show_results
     print "Ваши карты - #{@player.show_cards}, ваши очки - #{@player.current_points}\n"\
           "Карты диллера - #{@dealer.show_cards}, очки диллера - #{@dealer.current_points}\n"
     if (@player.current_points == @dealer.current_points) && (@player.current_points <= 21) && (@dealer.current_points <= 21)
@@ -60,28 +89,6 @@ class Interface
     else
       print "Вы проиграли\n"
       @bank.dealer_wins(@dealer)
-    end
-    @player.drop_cards
-    @dealer.drop_cards
-
-    if @player.bank.zero?
-      print "У вас закончились деньги, вы проиграли\n"
-      return nil
-    end
-
-    if @dealer.bank.zero?
-      print "У диллера закончились деньги, вы выиграли!\n"
-      return nil
-    end
-
-    print "Хотите сыграть еще раз?\n"\
-          "(0) - да, (1) - нет\n"
-    choice = gets.chomp
-    case choice
-    when '0'
-      game_start
-    when '1'
-      print "Выход\n"
     end
   end
 end
